131006613048416f3d005e703a41e2b9a7e1d769
haziqhalifi <haziq.halifi@gmail.com>
2025-05-11 21:25:39 +0800

    Add container component

--- src/pages/CarbonCalculator.jsx
+++ src/pages/CarbonCalculator.jsx
@@ -1,5 +1,14 @@
 function CarbonCalculator() {
-  return <div className="p-4">Carbon Footprint Calculator</div>;
+  return (
+    <div className="bg-light min-vh-100 py-8">
+      <div className="container mx-auto my-8">
+        <div className="bg-white rounded-xl shadow-xl p-6 md:p-8">
+          <h1 className="mb-4 text-center">Carbon Footprint Calculator</h1>
+          {/* Calculator content goes here */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CarbonCalculator;
